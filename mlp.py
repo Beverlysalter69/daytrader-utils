60ebe974ce9e2f65c04dfdcb36820b79e46803a6
Corey Auger <coreyauger@gmail.com>
2018-05-08 23:15:59 -0700

    0.32

--- mlp.py
+++ mlp.py
@@ -2,45 +2,60 @@ import numpy as np
 import os
 import dtdata as dt
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, Adam
+
 
 
 # fix random seed for reproducibility
 np.random.seed(90210)
 
 num_classes = 5
-batch_size = 128
-epochs = 200
+batch_size = 512
+epochs = 2000
+
+input_size = 350
 
 subset = -1 # -1 to use the entire data set
 
 path =r'/home/suroot/Documents/train/daytrader/ema-crossover' # path to data
 
-data = dt.loadData(path, subset)
+cache = "/tmp/daytrader_"+str(input_size)+".npy"
+labelsCache = "/tmp/daytrader_labels_"+str(input_size)+".npy"
+if( not os.path.isfile(cache) ):
+    data = dt.loadData(path, subset)
 
-(data, labels) = dt.centerAroundEntry(data)
-print(data.shape)
-data_scaled = dt.scale(data)
-labels_classed = dt.toClasses(labels, num_classes)
+    (data, labels) = dt.centerAroundEntry(data)
+    print(data.shape)
+    data_scaled = dt.scale(data)
+    labels_classed = dt.toClasses(labels, num_classes)
 
-dt.printLabelDistribution(labels_classed)
+    dt.printLabelDistribution(labels_classed)
 
-x_train, x_test, y_train, y_test = train_test_split(data_scaled, labels_classed, test_size=0.2)
+    pca = PCA(n_components=input_size, svd_solver='full')
+    data_reduced = pca.fit_transform(data_scaled) 
+    np.save(cache, data_reduced)
+    np.save(labelsCache, labels_classed)
+    
+data = np.load(cache)
+labels_classed = np.load(labelsCache)
+
+
+x_train, x_test, y_train, y_test = train_test_split(data, labels_classed, test_size=0.1)
 
 model = Sequential()
-model.add(Dense(1024, activation='relu', input_dim=2400))
-model.add(Dense(512, activation='relu'))
+model.add(Dense(32, activation='relu', input_dim=input_size))
 model.add(Dropout(0.2))
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.2))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.4))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
+              optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train,
